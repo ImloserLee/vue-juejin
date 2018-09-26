ba27813cfe081ed4077d072d5222f99f37ad3c33
lijie <1323023839@qq.com>
2018-09-26 22:52:09 +0800

    search add

--- src/pages/search/Search.vue
+++ src/pages/search/Search.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="search">
-    我是搜索页
+    <header class="header">
+      <input type="text" placeholder="搜索" />
+    </header>
   </div>
 </template>
 
@@ -11,5 +13,22 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  @import '~style/mixin.less';
+  .search {
+    width: 100%;
+    height: 100%;
+    .header {
+      // padding: 20px;
+      .flex();
+      height: 100px;
+      input {
+        width: 95%;
+        height: 60px;
+        font-size: 28px;
+        border: none;
+        border-radius: 8px;
+        background-color: #dcdde1;
+      }
+    }
+  }
 </style>
